b28ff0352e21d7e63629c5e6719e06a7406a35a3
Aaron Nartey <aaronnartey001@gmail.com>
2026-07-26 12:15:40 +0000

    test: domain testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Page = () => {
   return (
     <main className="w-full min-h-fit flex flex-col justify-between">
-
+      <p className='mx-auto font-bold text-[100px] text-green-200'>AgriTel Ghana</p>
     </main>
   )
 }
